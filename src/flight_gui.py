ea6a18127ebdc53973a88bb8fbbcf8db241c20ae
Sean Kim <sh32kim@uwaterloo.ca>
2018-11-09 06:51:44 -0500

    caption fields added

--- src/flight_gui.py
+++ src/flight_gui.py
@@ -8,7 +8,14 @@ import os
 
 class FlightGui:
     def _handle_keydown(self, evt):
+        global show_label, pause
         print('Got keydown', evt)
+        k = evt.key
+        if (k == 'l'):
+            show_label = not show_label
+        elif (k == 'p'):
+            pause = not pause
+
 
     def _handle_click(self, evt):
         print('Got click', evt)
@@ -27,6 +34,14 @@ class FlightGui:
 
         self._scene.bind('keydown', self._handle_keydown)
         self._scene.bind('click', self._handle_click)
+        self._scene.caption = ""
+        self._scene.append_to_caption("\n")
+        self._scene.append_to_caption("center:", "\n")
+        self._scene.append_to_caption("target:", "\n")
+        self._scene.append_to_caption("ref:", "\n")
+        self._scene.append_to_caption("NAVmode", "\n")
+
+
 
         self._spheres = {}
 
@@ -43,9 +58,16 @@ class FlightGui:
     def draw(self, physical_state_to_draw):
         for planet in physical_state_to_draw.entities:
             self._update_sphere(planet)
+            self._scene.caption = ""
+            self._scene.append_to_caption("\n")
+            self._scene.append_to_caption("center:", "XXX", "\n")
+            self._scene.append_to_caption("target:", "\n")
+            self._scene.append_to_caption("ref:", "\n")
+            self._scene.append_to_caption("NAVmode", "\n")
 
     def rate(self, framerate):
         self._vpython.rate(framerate)
+
 
     def _draw_sphere(self, planet):
         texture = os.path.join(self._texture_path, planet.name + '.jpg')
@@ -65,11 +87,14 @@ class FlightGui:
             )
 
     def _update_sphere(self, planet):
+
         self._spheres[planet.name].pos = self._vpython.vector(
             planet.x, planet.y, 0)
+
 
     def recentre_camera(self, planet_name):
         try:
             self._scene.camera.follow(self._spheres[planet_name])
         except KeyError:
             print('Unrecognized planet to follow:', planet_name)
+
