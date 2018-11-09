83d39b7aebc525ebdbd1150eb262cd78682c038d
Sean Kim <sh32kim@uwaterloo.ca>
2018-11-09 12:18:49 -0500

    captions added

--- src/flight_gui.py
+++ src/flight_gui.py
@@ -7,18 +7,97 @@ import os
 
 
 class FlightGui:
+
+    cur_caption = 0
+    caption_obj = ["Sun", "AYSE", "AYSE", "deprt ref"]
+
+    def set_caption(self):
+        if self.cur_caption == 0:
+            self._scene.caption = "\n"
+            self._scene.append_to_caption("<b>center:</b>", self.caption_obj[0], "\n")
+            self._scene.append_to_caption("target:", self.caption_obj[1], "\n")
+            self._scene.append_to_caption("ref:", self.caption_obj[2], "\n")
+            self._scene.append_to_caption("NAVmode:", self.caption_obj[3], "\n")
+        elif self.cur_caption == 1:
+            self._scene.caption = "\n"
+            self._scene.append_to_caption("center:", self.caption_obj[0], "\n")
+            self._scene.append_to_caption("<b>target:</b>", self.caption_obj[1], "\n")
+            self._scene.append_to_caption("ref:", self.caption_obj[2], "\n")
+            self._scene.append_to_caption("NAVmode:", self.caption_obj[3], "\n")
+        elif self.cur_caption == 2:
+            self._scene.caption = "\n"
+            self._scene.append_to_caption("center:", self.caption_obj[0], "\n")
+            self._scene.append_to_caption("target:", self.caption_obj[1], "\n")
+            self._scene.append_to_caption("<b>ref:</b>", self.caption_obj[2], "\n")
+            self._scene.append_to_caption("NAVmode:", self.caption_obj[3], "\n")
+        elif self.cur_caption == 3:
+            self._scene.caption = "\n"
+            self._scene.append_to_caption("center:", self.caption_obj[0], "\n")
+            self._scene.append_to_caption("target:", self.caption_obj[1], "\n")
+            self._scene.append_to_caption("ref:", self.caption_obj[2], "\n")
+            self._scene.append_to_caption("<b>NAVmode:</b>", self.caption_obj[3], "\n")
+
     def _handle_keydown(self, evt):
         global show_label, pause
-        print('Got keydown', evt)
+        #print('Got keydown', evt)
         k = evt.key
         if (k == 'l'):
             show_label = not show_label
         elif (k == 'p'):
             pause = not pause
+        elif (k == 'e'):
+            self._scene.center = self._spheres['Earth'].pos
+            #show = label_earth.visible
+            #label_earth.visible = not show
+            #label_earth.pos = earth.pos
+        elif (k == 's'):
+            self._scene.center = self._spheres['Sun'].pos
+            #show = label_sun.visible
+            #label_sun.visible = not show
+            #label_sun.pos = sun.pos
+        elif (k == 't'):
+            self.cur_caption = (self.cur_caption + 1) % 4
+            self.set_caption()
 
+    def get_objname(self, obj):
+        for k, v in self._spheres.items():
+            if v == obj:
+                obj_name = k;
+        return obj_name
+
+    def update_caption(self, obj):
+        obj_name = self.get_objname(obj)
+        if self.cur_caption == 0:
+            # action: update the camera center
+            self.scene.center = obj.pos
+            self.caption_obj[0] = obj_name
+            self.set_caption()
+        if self.cur_caption == 1:
+            #action required
+            self.caption_obj[1] = obj_name
+            self.set_caption()
+
+        if self.cur_caption == 2:
+            #action required
+            self.caption_obj[2] = obj_name
+            self.set_caption()
+
+        if self.cur_caption == 3:
+            #action required
+            self.caption_obj[3] = obj_name
+            self.set_caption()
 
     def _handle_click(self, evt):
-        print('Got click', evt)
+        #print('Got click', evt)
+        # global obj, clicked
+        try:
+            obj = self._scene.mouse.pick
+            if obj != None:
+                self.update_caption(obj)
+
+        except AttributeError:
+            pass
+        #clicked = True
 
     def __init__(self, physical_state_to_draw, texture_path=None):
         import vpython  # Note that this might actually start an HTTP server!
@@ -34,14 +113,7 @@ class FlightGui:
 
         self._scene.bind('keydown', self._handle_keydown)
         self._scene.bind('click', self._handle_click)
-        self._scene.caption = ""
-        self._scene.append_to_caption("\n")
-        self._scene.append_to_caption("center:", "\n")
-        self._scene.append_to_caption("target:", "\n")
-        self._scene.append_to_caption("ref:", "\n")
-        self._scene.append_to_caption("NAVmode", "\n")
-
-
+        self.set_caption()
 
         self._spheres = {}
 
@@ -52,18 +124,13 @@ class FlightGui:
 
         for planet in physical_state_to_draw.entities:
             self._spheres[planet.name] = self._draw_sphere(planet)
+            #self._spheres[planet.name] = self._label_sphere(planet)
             if planet.name == 'Sun':
                 self._scene.camera.follow(self._spheres[planet.name])
 
     def draw(self, physical_state_to_draw):
         for planet in physical_state_to_draw.entities:
             self._update_sphere(planet)
-            self._scene.caption = ""
-            self._scene.append_to_caption("\n")
-            self._scene.append_to_caption("center:", "XXX", "\n")
-            self._scene.append_to_caption("target:", "\n")
-            self._scene.append_to_caption("ref:", "\n")
-            self._scene.append_to_caption("NAVmode", "\n")
 
     def rate(self, framerate):
         self._vpython.rate(framerate)
