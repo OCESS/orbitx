4c885d4307e547163115febe82ddb421ba962a92
Sean Kim <sh32kim@uwaterloo.ca>
2019-03-04 23:36:20 -0500

    added spacestation

--- orbitx/spacestation.py
+++ orbitx/spacestation.py
@@ -12,14 +12,37 @@ import math
 class SpaceStation(Displayable):
     def __init__(self, entity: protos.Entity, texture_path: Path) -> None:
         super(SpaceStation, self).__init__(entity, texture_path)
-        self._obj = vpython.sphere(	pos=calc.posn(entity),
-                                    axis=calc.ang_pos(entity.heading),
-                                    up=vpython.vector(0, 0, 1),
-                                    radius=entity.r,
-                                    make_trail=False,
-                                    retain=10000,
-                                    shininess=Displayable.PLANET_SHININIESS)
+        _pos = calc.posn(entity)
+        _radius = entity.r
+        _axis = 2 * entity.r * \
+                vpython.vector(np.cos(entity.heading), np.sin(entity.heading), 0)
+        ship = vpython.cone(pos=vpython.vector(2, 0, 0),
+                            axis=vpython.vector(-8, 0, 0),
+                            radius=3,
+                            opacity=0.7)
+        entrance = vpython.extrusion(path=[vpython.vec(0, 0, 0),
+                                           vpython.vec(-4, 0, 0)],
+                                     shape=[vpython.shapes.circle(radius=1.5),
+                                            vpython.shapes.rectangle(pos=[0, -0.3],
+                                                                     width=1.5,
+                                                                     height=1.5)],
+                                     pos=vpython.vec(0, 0, 0))  # position
+
+        self._obj = vpython.compound([ship, entrance])
+        self._obj.pos = _pos
+        self._obj.axis = _axis
+        self._obj.radius = _radius
+        self._obj.length = _radius
+        self._obj.height = _radius
+        self._obj.width = _radius
+
         self._obj.name = self._entity.name
+
+        self._station_trail = vpython.attach_trail(self._obj, retain=100)
+
+        self._station_trail.stop()
+        self._station_trail.clear()
+
         self._label = self._draw_labels()
     # end of __init__
 
@@ -34,9 +57,14 @@ class SpaceStation(Displayable):
     # end of draw
 
     def clear_trail(self) -> None:
-        trail = None
+        self._station_trail.clear()
+
     # end of clear_trail
 
     def trail_option(self, stop: bool = False) -> None:
-        trail = None
+        if stop:
+            self._station_trail.start()
+        else:
+            self._station_trail.stop()
+            self._station_trail.clear()
 # end of class SpaceStation
