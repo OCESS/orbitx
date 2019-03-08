3c3e9c693ab0eea2b2dbb816375178ff34100a74
goodday0404 <kjjeon@uwaterloo.ca>
2019-03-08 13:01:39 -0500

    #61 menu.py: creating menu & caption refactored

--- orbitx/flight_gui.py
+++ orbitx/flight_gui.py
@@ -25,6 +25,7 @@ from orbitx.planet import Planet
 from orbitx.habitat import Habitat
 from orbitx.spacestation import SpaceStation
 from orbitx.star import Star
+from orbitx.menu import Menu
 
 log = logging.getLogger()
 
@@ -66,6 +67,8 @@ class FlightGui:
         self._displaybles: Dict[str, Displayable] = {}
         # remove vpython ambient lighting
         self._scene.lights = []  # This line shouldn't be removed
+        self._wtexts = []
+        self._menu: vpython.menu = Menu()
         ################################################################
 
         self._scene.autoscale: bool = False
@@ -103,7 +106,7 @@ class FlightGui:
         Displayable._reference_landing_graphic = (
             Displayable._draw_landing_graphic(self._reference))
 
-        self._set_caption()
+        self._menu.set_caption(self)
 
         # Add an animation when launching the program
         #   to describe the solar system and the current location
@@ -147,101 +150,6 @@ class FlightGui:
         _scene.range: float = 696000000.0 * 15000  # Sun radius * 15000
         return _scene
     # end of _init_canvas
-
-    def _set_caption(self) -> None:
-        """Set and update the captions."""
-
-        # There's a bit of magic here. Normally, vpython.wtext will make a
-        # <div> in the HTML and automaticall update it when the .text field is
-        # updated in this python code. But if you want to insert a wtext in the
-        # middle of a field, the following first attempt won't work:
-        #     scene.append_to_caption('<table>')
-        #     vpython.wtext(text='widget text')
-        #     scene.append_to_caption('</table>')
-        # because adding the wtext will also close the <table> tag.
-        # But you can't make a wtext that contains HTML DOM tags either,
-        # because every time the text changes several times a second, any open
-        # dropdown menus will be closed.
-        # So we have to insert a <div> where vpython expects it, manually.
-        # We take advantage of the fact that manually modifying scene.caption
-        # will remove the <div> that represents a wtext. Then we add the <div>
-        # back, along with the id="x" that identifies the div, used by vpython.
-        #
-        # TL;DR the div_id variable is a bit magic, if you make a new wtext
-        # before this, increment div_id by one..
-        self._scene.caption += "<table>\n"
-        self._wtexts = []
-        div_id = 1
-        for caption, text_gen_func, helptext, new_section in [
-            ("Orbit speed",
-             lambda: f"{calc.orb_speed(self._reference):,.7g} m/s",
-             "Speed required for circular orbit at current altitude",
-             False),
-            ("Periapsis",
-             lambda: f"{calc.periapsis(self._reference):,.7g} m",
-             "Lowest altitude in naïve orbit around reference",
-             False),
-            ("Apoapsis",
-             lambda: f"{calc.apoapsis(self._reference):,.7g} m",
-             "Highest altitude in naïve orbit around reference",
-             False),
-            ("Ref alt",
-             lambda: f"{calc.altitude(self._reference, self._habitat):,.7g} m",
-             "Altitude of habitat above reference surface",
-             True),
-            ("Ref speed",
-             lambda: f"{calc.speed(self._reference, self._habitat):,.7g} m/s",
-             "Speed of habitat above reference surface",
-             False),
-            ("Vertical speed",
-             lambda:
-             f"{calc.v_speed(self._reference, self._habitat):,.7g} m/s ",
-             "Vertical speed of habitat towards/away reference surface",
-             False),
-            ("Horizontal speed",
-             lambda:
-             f"{calc.h_speed(self._reference, self._habitat):,.7g} m/s ",
-             "Horizontal speed of habitat across reference surface",
-             False),
-            ("Targ alt",
-             lambda:
-             f"{calc.altitude(self._target, self._habitat):,.7g} m",
-             "Altitude of habitat above reference surface",
-             True),
-            ("Targ speed",
-             lambda:
-             f"{calc.speed(self._target, self._habitat):,.7g} m/s",
-             "Altitude of habitat above reference surface",
-             False),
-            ("Throttle",
-             lambda:
-             f"{self._habitat.throttle:.1%}",
-             "Percentage of habitat's maximum rated engines",
-             True)
-        ]:
-            self._wtexts.append(vpython.wtext(text=text_gen_func()))
-            self._wtexts[-1].text_func = text_gen_func
-            self._scene.caption += f"""<tr>
-                <td {"class='newsection'" if new_section else ""}>
-                    {caption}
-                </td>
-                <td class="num{" newsection" if new_section else ""}">
-                    <div id="{div_id}">{self._wtexts[-1].text}</div>
-                </td>
-                <td class="helptext{" newsection" if new_section else ""}">
-                    {helptext}
-                </td>
-                </tr>\n"""
-            div_id += 1
-        self._scene.caption += "</table>"
-        self._set_menus()
-        self._scene.append_to_caption(style.HELP_CHECKBOX)
-        self._scene.append_to_caption(" Help text")
-        self._scene.append_to_caption(style.INPUT_CHEATSHEET)
-
-        self._scene.append_to_caption(style.VPYTHON_CSS)
-        self._scene.append_to_caption(style.VPYTHON_JS)
-    # end of _set_caption
 
     def recentre_camera(self, planet_name: str) -> None:
         """Change camera to focus on different object
@@ -396,71 +304,55 @@ class FlightGui:
 
     # TODO: create bind functions for target, ref, and NAV MODE
 
-    def _set_menus(self) -> None:
-        """This creates dropped down menu which is used when set_caption."""
+    def get_cpation_anchor(self) -> vpython.canvas:
+        return self._scene.caption_anchor
+    # end of get_cpation_anchor
 
-        def build_menu(*, choices: List[str] = None, bind: Callable = None,
-                       selected: str = None, caption: str = None,
-                       helptext: str = None) -> vpython.menu:
+    def get_spheres(self) -> List[protos.Entity]:
+        return self._spheres
+    # end of get_spheres
 
-            menu = vpython.menu(
-                choices=choices,
-                pos=self._scene.caption_anchor,
-                bind=bind,
-                selected=selected)
-            self._scene.append_to_caption(f"&nbsp;<b>{caption}</b>&nbsp;")
-            self._scene.append_to_caption(
-                f"<span class='helptext'>{helptext}</span>")
-            self._scene.append_to_caption("\n")
-            return menu
+    def get_reference(self) -> protos.Entity:
+        return self._reference
+    # end of get_reference
 
-        self._centre_menu = build_menu(
-            choices=list(self._spheres),
-            bind=self._recentre_dropdown_hook,
-            selected=DEFAULT_CENTRE,
-            caption="Centre",
-            helptext="Focus of camera"
-        )
+    def get_target(self) -> protos.Entity:
+        return self._target
+    # end of get_target
 
-        build_menu(
-            choices=list(self._spheres),
-            bind=lambda selection: self._set_reference(selection.selected),
-            selected=DEFAULT_REFERENCE,
-            caption="Reference",
-            helptext=(
-                "Take position, velocity relative to this.")
-        )
+    def get_origin(self) -> protos.Entity:
+        return self._origin
+    # end of get_origin
 
-        build_menu(
-            choices=list(self._spheres),
-            bind=lambda selection: self._set_target(selection.selected),
-            selected=DEFAULT_TARGET,
-            caption="Target",
-            helptext="For use by NAV mode"
-        )
+    def get_habitat(self) -> protos.Entity:
+        return self._habitat
+    # end of get_habitat
 
-        build_menu(
-            choices=['deprt ref'],
-            bind=lambda selection: self._set_navmode(selection.selected),
-            selected='deprt ref',
-            caption="NAV mode",
-            helptext="Automatically points habitat"
-        )
+    def append_caption(self, caption: str) -> None:
+        self._scene.append_to_caption(caption)
+    # end of append_caption
 
-        self._time_acc_menu = build_menu(
-            choices=[f'{n:,}×' for n in
-                     [1, 5, 10, 50, 100, 1_000, 10_000, 100_000]],
-            bind=self._time_acc_dropdown_hook,
-            selected=1,
-            caption="Warp",
-            helptext="Speed of simulation"
-        )
+    def concat_caption(self, caption: str) -> None:
+        self._scene.caption += caption
+    # end of concat_caption
 
-        self._scene.append_to_caption("\n")
-        vpython.checkbox(
-            bind=self._trail_checkbox_hook, checked=False, text='Trails')
-        self._scene.append_to_caption(
-            " <span class='helptext'>Graphically intensive</span>")
+    def append_wtexts(self, wtext: vpython.wtext) -> None:
+        self._wtexts.append(wtext)
+    # end of set_wtexts
+
+    def set_wtexts_text_func_at(self, index: int, text_func) -> None:
+        self._wtexts[index].text_func = text_func
+
+    def wtexts_at(self, index: int) -> vpython.wtext:
+        return self._wtexts[index]
+
+    def set_centre_menu(self, menu: vpython.menu) -> None:
+        self._centre_menu = menu
+    # end of _set_centre_menu
+
+    def set_time_acc_menu(self, menu: vpython.menu) -> None:
+        self._time_acc_menu = menu
+    # end of set_time_acc_menu
 
     def draw(self, physical_state_to_draw: protos.PhysicalState) -> None:
         self._last_physical_state = physical_state_to_draw
