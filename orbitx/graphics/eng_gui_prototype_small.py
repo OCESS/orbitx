d522107ffb07b539b763e564b1782cdcdfeb6bda
Stefan <stefandeyoung@gmail.com>
2020-07-15 21:00:07 -0400

    keybinds work in small eng_gui_p

--- orbitx/graphics/eng_gui_prototype_small.py
+++ orbitx/graphics/eng_gui_prototype_small.py
@@ -13,11 +13,20 @@ images = {}
 style = cw.Style('flat')
 
 
+keybinding = {'i': 'INS',
+           'c': 'CHUTE',
+           's': 'SRB'}
+
+
+def keybinds(event):
+    widgets['event_display'].configure(text=event.char)
+    try:
+        widgets[keybinding[event.char]].invoke()
+    except KeyError:
+        print('key does not correspond to a widget')
+
+
 class MainApplication(tk.Tk):
-    """The main application window in which lives the HabPage, and potentially
-    other future pages.
-    Creates the file menu
-    """
 
     def __init__(self):
         super().__init__()
@@ -45,7 +54,7 @@ class HabPage(tk.Frame):
 
         # A couple of IndicatorButtons
         widgets['INS'] = cw.Indicator(frame, text='INS', style=style)
-        widgets['LOS'] = cw.Indicator(frame, text='RAD', style=style)
+        widgets['LOS'] = cw.Indicator(frame, text='LOS', style=style)
 
         # A couple of OneTimeButtons
         widgets['SRB'] = cw.OneTimeButton(frame, text='SRB', style=style)
@@ -75,22 +84,9 @@ class HabPage(tk.Frame):
         widgets['event_display'].grid(row=3, column=0, padx=5, pady=40,
                                       columnspan=4)
 
-        # Keybindings
-        def keybinds(event):
-            print('HANDLER!')
-            event_params = (event.type, event.widget, event.x, event.y,
-                            event.x_root, event.y_root, event.char,
-                            event.keysym, event.keycode)
-            event_str = ' '.join(event_params)
-            print(event_str)
-            widgets['event_display'].configure(text=event_str)
-
-        widgets['INS'].bind_all('<Key>', lambda e: keybinds(e))
-        widgets['event_display'].bind('<Button-1>', lambda e: keybinds(e))
-        widgets['SRB'].bind_all('s', lambda e: widgets['SRB'].invoke())
-
 
 # MAIN
 app = MainApplication()    # Essential. Do not remove.
+app.bind_all('<Key>', lambda e: keybinds(e))
 widgets['a_asteroid'].alert()
 app.mainloop()    # Essential. Do not remove.
