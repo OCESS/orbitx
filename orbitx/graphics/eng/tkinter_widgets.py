b4e2cf92f845640211ae8430c62cea9d39be2036
Patrick Melanson <pmelanson@users.noreply.github.com>
2021-04-25 16:12:37 -0700

    Add better documentation and example for the Redrawable widget.

--- orbitx/graphics/eng/tkinter_widgets.py
+++ orbitx/graphics/eng/tkinter_widgets.py
@@ -4,7 +4,7 @@ Custom TKinter Widgets, reuseable across different Engineering views.
 Every widget should inherit from Redrawable.
 """
 
-from ABC import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod
 from typing import List
 
 from orbitx.data_structures import EngineeringState
@@ -12,7 +12,23 @@ from orbitx.data_structures import EngineeringState
 
 class Redrawable(metaclass=ABCMeta):
     """Inherit all custom widgets from this. Automatically registers instantiated
-    objects that inherit from this, which makes life easier."""
+    objects that inherit from this, which makes life easier.
+
+    Usage:
+
+        class MyWidget(Redrawable):
+            def __init__(self, blah, blooh, blah):
+                [your widget code here]
+                self._changeable_field_or_whatever = tk.StringVar()
+
+            def redraw(self, state: EngineeringState):
+                # If you forget to define this method, Python will give you an error.
+                # Just an example of using a tkinter StringVar
+                self._changeable_field_or_whatever.set(state.habitat_fuel)
+
+        MyWidget(arguments)
+        # That's it! main_window.py will automatically call redraw() on your widget!
+    """
 
     # Contains all instantiated Redrawables. Loop over this to redraw them all.
     all_instantiated_widgets: List['Redrawable'] = []
